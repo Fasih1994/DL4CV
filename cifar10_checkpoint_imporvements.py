7a79fef041f8941fdf2eb7c68b4af7d87b6c8bf1
Fasih1994 <fasihuddin1994@gmail.com>
2019-09-23 19:15:53 +0500

    added model_checkpointing

---
+++ cifar10_checkpoint_imporvements.py
@@ -0,0 +1,40 @@
+from keras.callbacks import ModelCheckpoint
+from sklearn.preprocessing import LabelBinarizer
+from pyimagesearch.nn.conv import MiniVGGNet
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+import argparse
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-w', '--weights', required=True,
+                help='path to weights directory')
+args = vars(ap.parse_args())
+
+print('[INFO] loading cifar10 data...')
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+trainX = trainX.astype('float') / 255.0
+testX = testX.astype('float') / 255.0
+
+print("[INFO] compiling model...")
+
+opt = SGD(lr=0.01, momentum=0.9, decay=0.01 / 40, nesterov=True)
+
+model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
+model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+# construct the callback to save only the *best* model to disk
+# based on the validation loss
+fname = os.path.sep.join([args['weights'], "weights-{epoch:03d}-{val_loss:.4f}.hdf5"])
+checkpoints = [ModelCheckpoint(filepath=fname, monitor='val_loss', mode='min',
+                               save_best_only=True, verbose=1)]
+callbacks = [checkpoints]
+
+print('[INFO] training model...')
+model.fit(trainX, trainY, validation_data=(testX, testY),
+          epochs=40, batch_size=64, callbacks=callbacks,verbose=2)
