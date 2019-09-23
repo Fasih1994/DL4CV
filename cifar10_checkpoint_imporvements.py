9b4e6908b79bc7d77c37da3dd9be0e7da008a900
Fasih1994 <fasihuddin1994@gmail.com>
2019-09-23 19:45:44 +0500

    removed

--- cifar10_checkpoint_imporvements.py
+++ cifar10_checkpoint_imporvements.py
@@ -23,18 +23,18 @@ testX = testX.astype('float') / 255.0
 
 print("[INFO] compiling model...")
 
-opt = SGD(lr=0.01, momentum=0.9, decay=0.01 / 40, nesterov=True)
+opt = SGD(lr=0.01, momentum=0.9, decay=0.01/40, nesterov=True)
 
 model = MiniVGGNet.build(width=32, height=32, depth=3, classes=10)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 # construct the callback to save only the *best* model to disk
 # based on the validation loss
-fname = os.path.sep.join([args['weights'], "weights-{epoch:03d}-{val_loss:.4f}.hdf5"])
-checkpoints = [ModelCheckpoint(filepath=fname, monitor='val_loss', mode='min',
+file_name = os.path.sep.join([args['weights'], "weights-{epoch:03d}-{val_loss:.5f}.hdf5"])
+checkpoints = [ModelCheckpoint(filepath=file_name, monitor='val_loss', mode='min',
                                save_best_only=True, verbose=1)]
 callbacks = checkpoints
 
 print('[INFO] training model...')
 model.fit(trainX, trainY, validation_data=(testX, testY),
-          epochs=40, batch_size=64, callbacks=callbacks,verbose=2)
+          epochs=40, batch_size=64, callbacks=callbacks, verbose=1)
