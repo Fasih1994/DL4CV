ab0980b7fb60f5ff56e91df8ad3c2bc4ad0c6ac5
Fasih1994 <fasihuddin1994@gmail.com>
2019-09-23 14:00:02 +0500

    added monitoring callbacks

---
+++ pyimagesearch/callbacks/trainingmonitor.py
@@ -0,0 +1,61 @@
+from keras.callbacks import BaseLogger
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+
+
+class TrainingMonitor(BaseLogger):
+    def __init__(self, figPath, jsonPath=None, startAt=0):
+        # store the output path for the figure, the path to the JSON
+        # serialized file, and the starting epoch
+        super(TrainingMonitor, self).__init__()
+        self.figPath = figPath
+        self.jsonPath = jsonPath
+        self.startAt = startAt
+
+    def on_train_begin(self, logs={}):
+        # initialize history directory
+        self.H = {}
+
+        # if the json history file exisit load the training history
+        if self.jsonPath is not None:
+            if os.path.exists(self.jsonPath):
+                self.H = json.loads(open(self.jsonPath).read())
+
+                # if starting epoch is supplied
+                if self.startAt > 0:
+                    # loop over the entries in the history log and
+                    # trim any entries that are past the starting
+                    # epoch
+                    for k in self.H.keys():
+                        self.H[k] = self.H[k][:self.startAt]
+
+    def on_epoch_end(self, epoch, logs={}):
+        for (k, v) in logs.items():
+            l = self.H.get(k, [])
+            l.append(v)
+            self.H[k] = 1
+
+        if self.jsonPath is not None:
+            f = open(self.jsonPath, 'w')
+            f.write(json.dumps(self.H))
+            f.close()
+
+        # ensure that two epochs have been passed
+        if len(self.H['loss']) > 1:
+            # plot training loss and accuracy
+            N = np.arange(0, len(self.H['loss']))
+            plt.style.use('ggplot')
+            plt.figure()
+            plt.plot(N, self.H['loss'], label = 'training loss')
+            plt.plot(N, self.H['val_loss'], label='validation loss')
+            plt.plot(N, self.H['acc'], label='training accuracy')
+            plt.plot(N, self.H['val_acc'], label='validation accuracy')
+            plt.title("Training Loss and Accuracy [Epoch {}]".format(len(self.H["loss"])))
+
+            plt.xlabel("Epochs #")
+            plt.ylabel("Loss/Accuracy")
+            plt.legend()
+            plt.savefig(self.figPath)
+            plt.close()
